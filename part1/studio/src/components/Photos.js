249ace8a71b4b0845cad8dbff707f96045b77e16
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-08-06 18:11:17 -0500

    .

--- part1/studio/src/components/Photos.js
+++ part1/studio/src/components/Photos.js
@@ -1,8 +1,12 @@
 import styles from './Description.module.css';
 export default function RecipePhoto() {
+  return(
   <img
     src="https://www.fifteenspatulas.com/wp-content/uploads/2016/06/Homemade-Sushi-1-640x427.jpg"
     alt="picture of sushi roll with salmon, cream cheese and avocado"
+    height="500px"
+    width="500px"
     className={styles.imageUpdates}
-  />;
+  />
+  );
 }
